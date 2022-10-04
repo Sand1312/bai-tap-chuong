3fe4ea00c93fe565b08113d16bce1c98293e4ccb
Sand1312 <87612288+Sand1312@users.noreply.github.com>
2022-10-04 22:11:36 +0700

    Create bai3.cpp

---
+++ bai3.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+int32_t main(){
+ float a,b;
+ std::cout<<"So thu 1: ";std::cin>>a;b+=a;
+ std::cout<<"So thu 2: ";std::cin>>a;b+=a;
+ std::cout<<"So thu 3: ";std::cin>>a;b+=a;
+ std::cout<<"Gia tri trung binh cua 3 so: "<<b/3;
+}
