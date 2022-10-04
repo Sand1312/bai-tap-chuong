bab1fc6323ce047a499ecd6738bc2a16d62f28ae
Sand1312 <87612288+Sand1312@users.noreply.github.com>
2022-10-04 22:07:32 +0700

    Create bai2.cpp

---
+++ bai2.cpp
@@ -0,0 +1,6 @@
+#include "iostream"
+int32_t main(){
+  std::cout<<"Cuong do dong dien: ";int I;std::cin>>I;
+  std::cout<<"Gia tri dien tro R: ";int R;std::cin>>R;
+  std::cout<<"Hieu dien the U: "<<(long long)I*R<<"(V)";
+}
