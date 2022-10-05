3298c68e7b9dafa2ea2d43a3a5348c21c77e3d6d
Sand1312 <87612288+Sand1312@users.noreply.github.com>
2022-10-05 08:43:37 +0700

    Create bai7.cpp

---
+++ chuong 1/bai7.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+signed main(){
+    int a,S{};
+    cout<<"Day lon: ";cin>>a;S+=a;
+    cout<<"Day nho: ";cin>>a;S+=a;
+    cout<<"Chieu cao: ";cin>>a;
+    cout<<"Dien tich hinh thang: "<<S*a/2;
+}
