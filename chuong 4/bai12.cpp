b8f6cb25f1087c2bae230eb11827c9c3ed2c47d5
Sandd <87612288+Sand1312@users.noreply.github.com>
2022-10-05 18:32:04 +0700

    Update and rename bai12.cpp to chuong 4/bai12.cpp

--- chuong 4/bai12.cpp
+++ chuong 4/bai12.cpp
@@ -1,37 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int timkiem(int A[], int N, int k) {
-    for (int i = 0; i < N; i++) {
-        if (A[i] == k) {
-            return k;
-        }
-    }
-
+    for (int i = 0; i < N; i++) 
+        if (A[i] == k) return k;
     return -1;
 }
-
 int main() {
     const int n = 100;
     int arr[n];
     int count = 0;
     for(int i = 0; i < n; i++) {
-        if (count == 20) {
-            break;
-        }
+        if (count == 20) break;
         do {
             cout << "Nhap so nguyen thu " << i << " (>= 10, <= 100) :";
             cin >> arr[i];
         }
         while (!(arr[i] >= 10 && arr[i] <= 100));
-        
         if(timkiem(arr, n, arr[i]) == -1) {
             count++;
-            for(int i = 0; i < count; i++) {
-                cout << arr[i] << " ";
-            }
+            for(int i = 0; i < count; i++)  cout << arr[i] << " ";
         }
     }
-
-    return 0;
 }
