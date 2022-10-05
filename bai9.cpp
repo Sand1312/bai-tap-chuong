046a2fc5df7fe37ff536a40b2de1e98012f04d15
Sandd <87612288+Sand1312@users.noreply.github.com>
2022-10-05 18:25:40 +0700

    Update bai9.cpp

--- bai9.cpp
+++ bai9.cpp
@@ -1,20 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 void nhap(int a[], int &N) {
     cout << "Nhap " << N << " gia tri cho mang: " << endl;
-    for(int i = 0; i < N; i++) {
-        cin >> a[i];
-    }
+    for(int i = 0; i < N; i++)  cin >> a[i];
 }
-
 void xuat(int a[], int N) {
-    for(int i = 0; i < N; i++) {
-        cout << a[i] << " ";
-    }
+    for(int i = 0; i < N; i++) cout << a[i] << " ";
 }
-
-
 int main() {
     int arr[10000];
     int n;
