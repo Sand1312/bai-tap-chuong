277b5c330d27a4f98ccb6d63d3a9e227a0d9a156
Sandd <87612288+Sand1312@users.noreply.github.com>
2022-10-05 18:38:38 +0700

    Update bai2.cpp

--- chuong 2/bai2.cpp
+++ chuong 2/bai2.cpp
@@ -1,17 +1,9 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include "math.h"
 using namespace std;
-
-void Bai2(){
-	double x1,x2,y1,y2,x,y,d;
-	cout<<"Nhap vao toa do 2 diem: ";
-	cin>>x1>>y1>>x2>>y2;
-	x=(x1+x2)/2;
-	y=(y1+y2)/2;
-	d= sqrt((abs(x1-x2))*(abs(x1-x2))+(abs(y1-y2))*(abs(y1-y2)));
-	cout<<"Toa do trung diem la: "<<x<<" "<<y<<endl;
-	cout<<"Do dai doan thang la: "<<d;
- 
-}
-int main(){
-    Bai2();
+signed main(){
+    float x1,y1,x2,y2;
+    cout<<"Toa do diem thu nhat: ";cin>>x1>>y1;
+    cout<<"Toa do diem thu hai: ";cin>>x2>>y2;
+    cout<<"Do dai doan thang ("<<x1<<","<<y1<<"),("<<x2<<","<<y2<<") la: "<<sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
