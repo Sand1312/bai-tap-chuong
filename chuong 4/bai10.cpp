b06e7b70c7a4108af9a88f8aa1609bc746878c78
Sandd <87612288+Sand1312@users.noreply.github.com>
2022-10-05 18:28:07 +0700

    Update and rename bai10.cpp to chuong 4/bai10.cpp

--- chuong 4/bai10.cpp
+++ chuong 4/bai10.cpp
@@ -1,47 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 void nhap(int *a, int &n) {
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
+    for(int i = 0; i < n; i++) cin >> a[i];
 }
-
 void xuat(int *a, int &n) {
-    for(int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
+    for(int i = 0; i < n; i++) cout << a[i] << " ";
 }
-
-void xuatchan(int *a, int &n)
-{
+void xuatchan(int *a, int &n){
     for(int i=0;i<n;i++)
-    {
         if(a[i]%2==0) cout << a[i] << " ";
-    }
     cout << endl;
 }
-void xuatVTchan(int *a, int &n)
-{
+void xuatVTchan(int *a, int &n){
     for(int i=0;i<n;i++)
-    {
         if(i%2==0) cout << a[i] << " ";
-    }
     cout << endl;
 }
-
-void thanhle(int *a, int &n)
-{
-    for(int i=0;i<n;i++)
-    {
-        if(a[i]%2==1)
-        {
-            a[i]++;
-        }
+void thanhle(int *a, int &n){
+    for(int i=0;i<n;i++){
+        if(a[i]%2==1) a[i]++;
         cout << a[i] << " ";
     }
 }
-
 int main() {
     int a[10000];
     int n;
@@ -51,6 +31,4 @@ int main() {
     xuatchan(a, n);
     xuatVTchan(a, n);
     thanhle(a, n);
-
-    return 0;   
 }
