72ea15e0992eb16dd7d9a51938d676baf1d4211c
Sandd <87612288+Sand1312@users.noreply.github.com>
2022-10-05 18:13:12 +0700

    Update and rename bai1.cpp to chuong 4/bai1.cpp

--- chuong 4/bai1.cpp
+++ chuong 4/bai1.cpp
@@ -1,7 +1,3 @@
-/* Ho va ten: Trinh Huy Hoang
-MSV: N21DCCN124
-Lop: D21CQCN02-N
-*/
 #include <bits/stdc++.h>
 using namespace std;
 void toLowerCase() {
@@ -9,67 +5,60 @@ void toLowerCase() {
     string s1 = "";
     string s;
 
-    cout << "Nhập một chuỗi: ";
+    cout << "Nhap mot chuoi: ";
     cin >> s;
-
-    for (int i = 0; i < s.length() ; i++) {
-        s1 += tolower(s[i]);
-    }
-
-    cout << "Chuỗi sau chữ thường: " << s1 << endl;
+    for (int i = 0; i < s.length() ; i++)    s1 += tolower(s[i]);
+    cout << "Chuoi sau chu thuong: " << s1 << endl;
 }
-
 void solvingFirstDegreeEquations() {
     float a, b;
-    cout << "Nhập 2 tham số của phương trình ax + b = 0:";
+    cout << "Nhap 2 tham so cua phuong trinh ax + b = 0:";
     cin >> a >> b;
     if (a != 0) {
         float x = - b / a;
-        cout << "Phương trình có nghiệm duy nhất x = " << x << endl;
+        cout << "Phuong trinh co nghiem duy nhat x = " << x << endl;
     }
     else {
         if (b != 0) {
-            cout << "Phương trình vô nghiệm." << endl;
+            cout << "Phuong trinh vo nghiem." << endl;
         }
         else {
-            cout << "Phương trình luôn đúng với mọi biến x." << endl;
+            cout << "Phuong trinh luon dung voi moi bien x." << endl;
         }
     }
 }
-
 void solvingQuadraticEquations() {
     float a, b, c;
-    cout << "Nhập 3 tham số của phương trình ax ^ 2 + bx + c = 0:";
+    cout << "Nhap 3 tham so của phuong trinh ax ^ 2 + bx + c = 0:";
     cin >> a >> b >> c;
-
     if( a != 0) {
         float delta = pow(b, 2) - 4 * a * c;
         if(delta < 0) {
-            cout << "Phương trình không có nghiệm!" << endl;
+            cout << "Phuong trinh khong co nghiem!" << endl;
         }
         else {
             if(delta == 0) {
                 float x = - b / (2 * a);
-                cout << "Phương trình có nghiệm duy nhất x = " << x << endl;
+                cout << "Phuong trinh co nghiem duy nhat x = " << x << endl;
             }
             else {
                 float x1 = (-b - sqrt(delta)) / (2 * a);
                 float x2 = (-b + sqrt(delta)) / (2 * a);
-                cout << "Phương trình có 2 nghiệm phân biệt là x1 = " << x1 << ", x2 = " << x2 << endl; 
+                cout << "Phuong trinh có 2 nghiem phan biet là x1 = " << x1 << ", x2 = " << x2 << endl; 
             }
         }
     }
     else {
         if(b != 0) {
             float x = -b / c;
-            cout << "Phương trình có nghiệm duy nhất x = " << x << endl;
+            cout << "Phuong trinh co nghiem duy nhat x = " << x << endl;
         }
         else {
             if(c != 0) {
-                cout << "Phương trình không có nghiệm!" << endl;
+                cout << "Phuong trinh khong co nghiem!" << endl;
             }
             else {
-                cout << "Phương trình luôn đúng với mọi biến x." << endl;
+                cout << "Phuong trinh luon đung voi moi bien x." << endl;
             }
         }
     }
@@ -77,17 +66,17 @@ void solvingQuadraticEquations() {
 
 void theMinOf4Int() {
     int a, b, c, d;
-    cout << "Nhập 4 số nguyên:";
+    cout << "Nhap 4 so nguyen:";
     cin >> a >> b >> c >> d;
     cout << min(min(a, b), min(c, d));
 }
 
 void swappingTwoValue() {
     int a,b;
-    cout << "Nhập hai số nguyên: ";
+    cout << "Nhap hai so nguyen: ";
     cin >> a >> b;
     cout << "a = " << a << " , b = " << b << endl;
-    cout << "Hoán đổi ..." << endl;
+    cout << "Hoan đoi ..." << endl;
     int c = a;
     a = b;
     b = c;
@@ -96,7 +85,7 @@ void swappingTwoValue() {
 
 void sortFourIntegersASC() {
     int arr[4];
-    cout << "Nhập 4 số nguyên:";
+    cout << "Nhap 4 số nguyen:";
 
     for(int i = 0; i < 4; i++) {
         cin >> arr[i];
@@ -107,7 +96,7 @@ void sortFourIntegersASC() {
     
     sort(arr, arr + n);
   
-    cout << endl << "Mảng sau khi sắp xếp bằng cách sử dụng sắp xếp mặc định là: \n";
+    cout << endl << "Mang sau khi sap xep bang cach su dung sap xep mac đinh là: \n";
     for (int i = 0; i < n; ++i)
         cout << arr[i] << " ";
 }
@@ -117,12 +106,12 @@ int main() {
     int n;
 
     do {
-        cout << endl << "1.Hàm đổi một ký tự hoa sang ký tự thường;" << endl;
-        cout << "2. Thủ tục giải phương trình bậc nhất;" << endl;
-        cout << "3.Thủ tục giải phương trình bậc hai;" << endl;
-        cout << "4. Hàm xác định giá trị nhỏ nhất của 4 số nguyên;" << endl;
-        cout << "5. Thủ tục hoán vị hai số nguyên;" << endl;
-        cout << "6. Thủ tục sắp xếp 4 số nguyên tăng dần." << endl;
+        cout << endl << "1.Ham đoi mot ky tu hoa sang ky tu thuong;" << endl;
+        cout << "2. Thu tuc giai phuong trinh bac nhat;" << endl;
+        cout << "3.Thu tuc giai phuong trinh bac hai;" << endl;
+        cout << "4. Ham xac đinh gia tri nho nhat cua 4 so nguyen;" << endl;
+        cout << "5. Thu tuc hoan vi hai so nguyen;" << endl;
+        cout << "6. Thu tuc sap xep 4 so nguyen tang dan." << endl;
         cout << "0. Exit program." << endl << endl;
  
         cout << "Chon cau hoi: ";
